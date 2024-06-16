de604d9813cf395d3f69742ad283ef0c3fde111c
V1N1C1U5ESPINDOLA <166178486+V1N1C1U5ESPINDOLA@users.noreply.github.com>
2024-06-15 22:21:52 -0300

    ok

--- Lista02/quest11-go/main.go
+++ Lista02/quest11-go/main.go
@@ -1,3 +1,29 @@
 package main
 
-//CALCULO DA RAIZ QUADRADA
+// CÁLCULO DA RAIZ QUADRADA
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("DIGITE OS NÚMEROS:")
+
+	var num, margin float64
+	fmt.Scan(&num, &margin)
+
+	xn := 1.0
+
+	for {
+		x := 0.5 * (xn + (num / xn))
+
+		err := (x*x - num)
+
+		fmt.Printf("r: %.9f erro: %.9f\n", x, err)
+
+		xn = x
+		if err < margin {
+			break
+		}
+	}
+}
